dae716ee8b7172c709d45d982d9f9157347af6ff
Karan Sharma <karansharma1295@gmail.com>
2015-07-20 19:23:39 +0530

    mp3 Download

--- download.py
+++ download.py
@@ -1,1 +1,20 @@
-__author__ = 'ninjaPython'
+__author__ = 'karan'
+from __future__ import unicode_literals
+import youtube_dl
+import sys
+
+URL=sys.argv[1]
+
+#download and strip to mp3
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([URL])
+
+
